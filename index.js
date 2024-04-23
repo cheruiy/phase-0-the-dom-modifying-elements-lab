c226dd17da069b40073637d449cd0be542151934
Cheruiyot Robert <kiruir38@gmail.com>
2024-04-23 19:54:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+// Find the main element with id 'main'
+const mainElement = document.getElementById('main');
+
+// Remove the main element if it exists
+if (mainElement) {
+    mainElement.remove();
+}
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// Set the inner text of the new header
+//newHeader.innerText = 'This is a new header';
+
+// Set the id of the new header
+newHeader.id = 'victory';
+
+// Set the inner text of the new header to include "is the champion" along with your name
+const yourName = 'Cheruiyot Robert';
+newHeader.innerText = `${yourName} is the champion`;
+
+// Add the new header to the document body
+document.body.appendChild(newHeader);
+
